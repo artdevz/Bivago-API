76a21af767cc017804dcb22d0b2bf674637b8ae5
artdevz <arthurdantaslacerda@gmail.com>
2025-08-18 20:51:25 -0300

    SignUp Impl

--- src/main/java/com/bivago_api/interfaces/controllers/AuthController.java
+++ src/main/java/com/bivago_api/interfaces/controllers/AuthController.java
@@ -1,18 +1,27 @@
 package com.bivago_api.interfaces.controllers;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.bivago_api.app.dto.auth.AuthRefreshDTO;
 import com.bivago_api.app.dto.auth.AuthResponseDTO;
 import com.bivago_api.app.dto.auth.AuthSigninDTO;
 import com.bivago_api.app.dto.auth.AuthSignupDTO;
+import com.bivago_api.app.dto.user.UserRequestDTO;
 import com.bivago_api.app.services.AuthService;
+import com.bivago_api.app.services.UserService;
+import com.bivago_api.domain.models.Role;
 import com.bivago_api.domain.models.User;
+import com.bivago_api.domain.repositories.IRoleRepository;
 import com.bivago_api.shared.utils.AsyncResultHandler;
 
 import jakarta.validation.Valid;
@@ -24,10 +33,15 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final AuthService authS;
+    private final UserService userS;
+    private final IRoleRepository roleR;
     
     @PostMapping("/signup")
     public ResponseEntity<AuthResponseDTO> signUp(@RequestBody @Valid AuthSignupDTO request) {
-        return null;
+        Role defaultRole = roleR.findByName("USER").orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Role USER não existe"));
+        UserRequestDTO dto = new UserRequestDTO(request, Set.of(defaultRole).stream().map(Role::getId).collect(Collectors.toSet()));
+        userS.create(dto);
+        return ResponseEntity.ok(AsyncResultHandler.await(authS.signin(new AuthSigninDTO(request.email(), request.password()))));
     }
 
     @PostMapping("/signin")
